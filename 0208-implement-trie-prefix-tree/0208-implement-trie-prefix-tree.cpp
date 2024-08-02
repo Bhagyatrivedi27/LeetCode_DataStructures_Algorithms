e17f29a6318e9d2e5c730eb521b5af5c0513f795
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-08-02 17:55:13 -0400

    Time: 47 ms (62.03%), Space: 48.4 MB (39.76%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,59 +1,58 @@
 struct trieNode{
-    trieNode() {
+    
+    trieNode(){
         isWord = false;
-        int i;
-        for(int i=0;i<26;i++)
+        for(int i=0;i<26;i++){
             next[i] = NULL;
+        }
     }
-    bool isWord;
-    trieNode *next [26];
+        bool isWord;
+        trieNode *next[26];
 };
-
 class Trie {
 public:
     trieNode *root;
     trieNode dummy;
+
     Trie() {
-        root = &dummy;
+        root = &dummy;       
     }
     
     void insert(string word) {
-        int n = word.size(), i;
+        int n = word.size();
         trieNode *curr = root;
-        
-        for(i=0;i<n;i++){
-            if(!curr->next[word[i]-'a']){
+        for(int i=0;i<n;i++){
+            if(!curr->next[word[i]-'a'])
                 curr->next[word[i]-'a'] = new trieNode;
-            }
+            
             curr = curr->next[word[i]-'a'];
         }
-        
         curr->isWord = true;
-        
     }
     
     bool search(string word) {
-        int n = word.size(), i;
+        int n = word.size();
         trieNode *curr = root;
-        
         for(int i=0;i<n;i++){
             if(!curr->next[word[i]-'a'])
                 return false;
+            
             curr = curr->next[word[i]-'a'];
         }
         return curr->isWord;
     }
     
     bool startsWith(string prefix) {
-          int n = prefix.size(), i;
+        
+        int n = prefix.size();
         trieNode *curr = root;
         
         for(int i=0;i<n;i++){
             if(!curr->next[prefix[i]-'a'])
                 return false;
+            
             curr = curr->next[prefix[i]-'a'];
         }
-        
         return true;
     }
 };
