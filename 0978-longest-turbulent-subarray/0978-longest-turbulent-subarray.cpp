fd46fe1333431baae455cc76aa9a91db6e9b4660
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-08-19 12:19:20 -0400

    Time: 57 ms (37.39%), Space: 46.5 MB (11.09%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int n= arr.size();
-        vector<int> incT(n);
-        vector<int> decT(n);
         
-        //Smallest problem
+        int n = arr.size();
+        vector<int>incT(n+1);
+        vector<int>decT(n+1);
+        int res=1;
         incT[0] = decT[0] = 1;
-        int res = 1;
-        for(int i=1;i<n;i++){
-            if(arr[i] > arr[i-1]){
-                incT[i] = 1+decT[i-1];
-                decT[i] = 1;
-            }
+        
+        for(int i=0;i<n;i++){
+            if(i==0)
+                continue;
             if(arr[i] < arr[i-1]){
-                decT[i] = 1+incT[i-1];
+                decT[i] = 1 + incT[i-1];
                 incT[i] = 1;
+            }
+            if(arr[i]>arr[i-1]){
+                incT[i] = 1+decT[i-1];
+                decT[i]=1;
             }
             if(arr[i] == arr[i-1]){
-                decT[i] = 1;
-                incT[i] = 1;
+                incT[i] = decT[i] = 1;
             }
-            res = max(res,max(decT[i], incT[i]));
+            res = max(res, max(incT[i], decT[i]));
         }
-        
         return res;
     }
 };
