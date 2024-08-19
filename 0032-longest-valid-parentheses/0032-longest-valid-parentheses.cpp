09fe110666d22419c9e24ea52b37e1843c77ef44
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-08-19 12:56:38 -0400

    Time: 0 ms (100.00%), Space: 9.2 MB (28.52%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        
-        int n = s.size(), res=0;
+        int n = s.size();
         if(n==0)
             return 0;
-        int i;
         vector<int>dp(n,0);
+        dp[0]= 0;
+        int res =0;
         
-        dp[0] = 0;
-        
-        for(i=1;i<n;i++){
+        for(int i=1;i<n;i++){
             if(s[i]=='(')
-                dp[i]=0;
+                dp[i] = 0;
             else{
                 if(i-dp[i-1]-1 >= 0 && s[i-dp[i-1]-1]=='('){
-                    dp[i] = 2+dp[i-1];
-                    
-                    if(i-dp[i-1]-2 >= 0)
-                        dp[i] += dp[i-dp[i-1]-2];       
+                    dp[i]= 2+ dp[i-1];
+                    if(i-dp[i-1]-2 >=0)
+                        dp[i] += dp[i-dp[i-1]-2];
                 }
             }
-            res = max(res,dp[i]);
+            res  =max(res, dp[i]);
+           
         }
-        return res;
-        
+         return res;
     }
 };
