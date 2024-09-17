ced187d1fc8a4cd62773da35910fdbe2d580cd52
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-09-16 23:02:13 -0400

    Time: 3 ms (79.52%), Space: 16.4 MB (35.78%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    void combinationSumHelper(vector<int>& candidates, int i, vector<int>& currSet, vector<vector<int>>& res, int target){
+    void combinationSumHelper(vector<int> &candidates, int target, int i, vector<int> &currSet, vector<vector<int>> &res){
+        
         int n = candidates.size();
         
         //base cases
-        if(target<0 or i>=n){
-            return;  
-        }
         
-        if(target==0){
+        if(target == 0 and i <=n){
             res.push_back(currSet);
             return;
         }
+        if(i>=n or target<0){
+            return;
+        }
         
-        //exclude
-        combinationSumHelper(candidates, i+1, currSet, res, target);
+        
+        //exclude 
+        combinationSumHelper(candidates, target, i+1, currSet, res);
         
         //include
         currSet.push_back(candidates[i]);
-        combinationSumHelper(candidates, i, currSet, res, target-candidates[i]);
+        combinationSumHelper(candidates, target-candidates[i], i, currSet, res);
         currSet.pop_back();
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> currSet;
+        
+        int n = candidates.size();
+        
+        vector<int>currSet;
         vector<vector<int>>res;
         
-        combinationSumHelper(candidates, 0, currSet, res, target);
+        combinationSumHelper(candidates, target, 0, currSet, res);
         return res;
+        
     }
 };
