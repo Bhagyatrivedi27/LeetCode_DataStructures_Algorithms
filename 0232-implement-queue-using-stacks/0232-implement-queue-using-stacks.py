ccca82b2e36444e2ec269cde01a74adf46caafb8
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-10-13 10:29:36 -0400

    Time: 36 ms (47.06%), Space: 17 MB (6.74%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,37 +1,23 @@
-############################################################
-# Write code in file solution.py 
-# Post solution.py in Canvas along with 4 screenshots that show Leetcode has passed.
-# Cut and paste the whole solution.py file in Leetcode and run. All tests must pass
-# You should do this 4 times for problems 225, 232, 622, and 641.
-# TA will run solution.py file 4 times for 225, 232, 622, and 641.
-# All tests must pass for 100% credit.
-###########################################################
-
 class ListNode:
-    #NOTHING CAN BE CHANGED HERE
+    # NOTHING CAN BE CHANGED HERE
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-         
-            
+
+
 ############################################################
 #  class  Slist
-###########################################################   
+###########################################################
 class Slist:
     def __init__(self):
-        #NOTHING CAN BE CHANGED HERE
+        # NOTHING CAN BE CHANGED HERE
         self._first = None
         self._last = None
-        self._len = 0 
-        
-    #############################
-    # WRITE All public functions BELOW
-    # YOU CAN HAVE ANY NUMBER OF PRIVATE FUNCTIONS YOU WANT
-    #############################
-    
-    def append(self, val):
-        # Append value to the list
-        new_node = ListNode(val)
+        self._len = 0
+
+    # Public method to add to the end of the list
+    def append(self, value):
+        new_node = ListNode(value)
         if self._first is None:
             self._first = new_node
             self._last = new_node
@@ -39,53 +25,87 @@ class Slist:
             self._last.next = new_node
             self._last = new_node
         self._len += 1
-    
+
+    # Public method to add to the front of the list
+    def prepend(self, value):
+        new_node = ListNode(value)
+        if self._first is None:
+            self._first = new_node
+            self._last = new_node
+        else:
+            new_node.next = self._first
+            self._first = new_node
+        self._len += 1
+
+    # Public method to remove and return the first element
     def remove_first(self):
-        # Remove first element of the list
-        if self._first is not None:
+        if self._first is None:
+            return None
+        value = self._first.val
+        self._first = self._first.next
+        self._len -= 1
+        if self._first is None:
+            self._last = None
+        return value
+
+    # Public method to remove the last element
+    def remove_last(self):
+        if self._first is None:
+            return None
+        if self._first == self._last:
             value = self._first.val
-            self._first = self._first.next
-            if self._first is None:
-                self._last = None
-            self._len -= 1
+            self._first = None
+            self._last = None
+            self._len = 0
             return value
-        return None
-    
+        current = self._first
+        while current.next != self._last:
+            current = current.next
+        value = self._last.val
+        self._last = current
+        self._last.next = None
+        self._len -= 1
+        return value
+
+    # Public method to get the first element's value
     def get_first(self):
-        # Get the first element's value
-        if self._first is not None:
-            return self._first.val
-        return None
-    
+        if self._first is None:
+            return -1
+        return self._first.val
+
+    # Public method to get the last element's value
     def get_last(self):
-        # Get the last element's value
-        if self._last is not None:
-            return self._last.val
-        return None
-    
+        if self._last is None:
+            return -1
+        return self._last.val
+
+    # Check if the list is empty
     def is_empty(self):
         return self._len == 0
-    
+
+    # Return the current length of the list
     def length(self):
         return self._len
-    
+
+    # Reset the list
     def reset(self):
-        # Reset the list
         self._first = None
         self._last = None
         self._len = 0
-    
+
+
 ############################################################
 #  class  MyStack
 # 225. Implement Stack using Queues
 # https://leetcode.com/problems/implement-stack-using-queues
-########################################################### 
+###########################################################
 class MyStack:
     def __init__(self):
+        # Use Slist for stack implementation
         self._s = Slist()
 
     def push(self, x: int) -> None:
-        self._s.append(x)
+        self._s.prepend(x)
 
     def pop(self) -> int:
         return self._s.remove_first()
@@ -101,9 +121,10 @@ class MyStack:
 #  class  MyQueue
 # 232. Implement Queue using Stacks
 # https://leetcode.com/problems/implement-queue-using-stacks/
-########################################################### 
+###########################################################
 class MyQueue:
     def __init__(self):
+        # Use Slist for queue implementation
         self._s = Slist()
 
     def push(self, x: int) -> None:
@@ -123,10 +144,10 @@ class MyQueue:
 #  MyCircularQueue
 # 622. Design Circular Queue
 # https://leetcode.com/problems/design-circular-queue/
-########################################################### 
+###########################################################
 class MyCircularQueue:
     def __init__(self, k: int):
-        self._K = k 
+        self._K = k
         self._s = Slist()
 
     def enQueue(self, value: int) -> bool:
@@ -158,23 +179,16 @@ class MyCircularQueue:
 #  MyCircularDeque
 # 641. Design Circular Deque
 # https://leetcode.com/problems/design-circular-deque
-###########################################################  
+###########################################################
 class MyCircularDeque:
     def __init__(self, k: int):
-        self._K = k 
+        self._K = k
         self._s = Slist()
 
     def insertFront(self, value: int) -> bool:
         if self.isFull():
             return False
-        new_node = ListNode(value)
-        if self._s._first is None:
-            self._s._first = new_node
-            self._s._last = new_node
-        else:
-            new_node.next = self._s._first
-            self._s._first = new_node
-        self._s._len += 1
+        self._s.prepend(value)
         return True
 
     def insertLast(self, value: int) -> bool:
@@ -192,15 +206,7 @@ class MyCircularDeque:
     def deleteLast(self) -> bool:
         if self.isEmpty():
             return False
-        if self._s._len == 1:
-            self._s.reset()
-            return True
-        current = self._s._first
-        while current.next != self._s._last:
-            current = current.next
-        current.next = None
-        self._s._last = current
-        self._s._len -= 1
+        self._s.remove_last()
         return True
 
     def getFront(self) -> int:
