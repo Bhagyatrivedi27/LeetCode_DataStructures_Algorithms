70cb0505d0e9d3685a0ed5b8eccad1ea489615ce
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-10-13 10:28:49 -0400

    Time: 38 ms (30.87%), Space: 17 MB (9.71%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,195 +1,222 @@
-# Definition for a singly linked list node (used in all solutions)
 class ListNode:
-    def __init__(self, value=None):
-        self.value = value
-        self.next = None
+    # NOTHING CAN BE CHANGED HERE
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-# Circular Queue Implementation using Singly Linked List
+############################################################
+#  class  Slist
+###########################################################
+class Slist:
+    def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._first = None
+        self._last = None
+        self._len = 0
+
+    # Public method to add to the end of the list
+    def append(self, value):
+        new_node = ListNode(value)
+        if self._first is None:
+            self._first = new_node
+            self._last = new_node
+        else:
+            self._last.next = new_node
+            self._last = new_node
+        self._len += 1
+
+    # Public method to add to the front of the list
+    def prepend(self, value):
+        new_node = ListNode(value)
+        if self._first is None:
+            self._first = new_node
+            self._last = new_node
+        else:
+            new_node.next = self._first
+            self._first = new_node
+        self._len += 1
+
+    # Public method to remove and return the first element
+    def remove_first(self):
+        if self._first is None:
+            return None
+        value = self._first.val
+        self._first = self._first.next
+        self._len -= 1
+        if self._first is None:
+            self._last = None
+        return value
+
+    # Public method to remove the last element
+    def remove_last(self):
+        if self._first is None:
+            return None
+        if self._first == self._last:
+            value = self._first.val
+            self._first = None
+            self._last = None
+            self._len = 0
+            return value
+        current = self._first
+        while current.next != self._last:
+            current = current.next
+        value = self._last.val
+        self._last = current
+        self._last.next = None
+        self._len -= 1
+        return value
+
+    # Public method to get the first element's value
+    def get_first(self):
+        if self._first is None:
+            return -1
+        return self._first.val
+
+    # Public method to get the last element's value
+    def get_last(self):
+        if self._last is None:
+            return -1
+        return self._last.val
+
+    # Check if the list is empty
+    def is_empty(self):
+        return self._len == 0
+
+    # Return the current length of the list
+    def length(self):
+        return self._len
+
+    # Reset the list
+    def reset(self):
+        self._first = None
+        self._last = None
+        self._len = 0
+
+
+############################################################
+#  class  MyStack
+# 225. Implement Stack using Queues
+# https://leetcode.com/problems/implement-stack-using-queues
+###########################################################
+class MyStack:
+    def __init__(self):
+        # Use Slist for stack implementation
+        self._s = Slist()
+
+    def push(self, x: int) -> None:
+        self._s.prepend(x)
+
+    def pop(self) -> int:
+        return self._s.remove_first()
+
+    def top(self) -> int:
+        return self._s.get_first()
+
+    def empty(self) -> bool:
+        return self._s.is_empty()
+
+
+############################################################
+#  class  MyQueue
+# 232. Implement Queue using Stacks
+# https://leetcode.com/problems/implement-queue-using-stacks/
+###########################################################
+class MyQueue:
+    def __init__(self):
+        # Use Slist for queue implementation
+        self._s = Slist()
+
+    def push(self, x: int) -> None:
+        self._s.append(x)
+
+    def pop(self) -> int:
+        return self._s.remove_first()
+
+    def peek(self) -> int:
+        return self._s.get_first()
+
+    def empty(self) -> bool:
+        return self._s.is_empty()
+
+
+############################################################
+#  MyCircularQueue
+# 622. Design Circular Queue
+# https://leetcode.com/problems/design-circular-queue/
+###########################################################
 class MyCircularQueue:
     def __init__(self, k: int):
-        self.max_size = k
-        self.size = 0
-        self.front = None
-        self.rear = None
+        self._K = k
+        self._s = Slist()
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
-
-        new_node = ListNode(value)
-
-        if self.isEmpty():
-            self.front = new_node
-            self.rear = new_node
-            new_node.next = self.front  # Circular link
-        else:
-            self.rear.next = new_node
-            self.rear = new_node
-            self.rear.next = self.front  # Maintain circularity
-
-        self.size += 1
+        self._s.append(value)
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
-
-        if self.front == self.rear:
-            self.front = None
-            self.rear = None
-        else:
-            self.front = self.front.next
-            self.rear.next = self.front
-
-        self.size -= 1
+        self._s.remove_first()
         return True
 
     def Front(self) -> int:
-        if self.isEmpty():
-            return -1
-        return self.front.value
+        return self._s.get_first() if not self.isEmpty() else -1
 
     def Rear(self) -> int:
-        if self.isEmpty():
-            return -1
-        return self.rear.value
+        return self._s.get_last() if not self.isEmpty() else -1
 
     def isEmpty(self) -> bool:
-        return self.size == 0
+        return self._s.is_empty()
 
     def isFull(self) -> bool:
-        return self.size == self.max_size
+        return self._s.length() == self._K
 
 
-# Circular Deque Implementation using Singly Linked List
+############################################################
+#  MyCircularDeque
+# 641. Design Circular Deque
+# https://leetcode.com/problems/design-circular-deque
+###########################################################
 class MyCircularDeque:
     def __init__(self, k: int):
-        self.max_size = k
-        self.size = 0
-        self.front = None
-        self.rear = None
+        self._K = k
+        self._s = Slist()
 
     def insertFront(self, value: int) -> bool:
         if self.isFull():
             return False
-
-        new_node = ListNode(value)
-
-        if self.isEmpty():
-            self.front = new_node
-            self.rear = new_node
-            new_node.next = self.front  # Circular link
-        else:
-            new_node.next = self.front
-            self.front = new_node
-            self.rear.next = self.front  # Maintain circularity
-
-        self.size += 1
+        self._s.prepend(value)
         return True
 
     def insertLast(self, value: int) -> bool:
         if self.isFull():
             return False
-
-        new_node = ListNode(value)
-
-        if self.isEmpty():
-            self.front = new_node
-            self.rear = new_node
-            new_node.next = self.front  # Circular link
-        else:
-            self.rear.next = new_node
-            self.rear = new_node
-            self.rear.next = self.front  # Maintain circularity
-
-        self.size += 1
+        self._s.append(value)
         return True
 
     def deleteFront(self) -> bool:
         if self.isEmpty():
             return False
-
-        if self.front == self.rear:
-            self.front = None
-            self.rear = None
-        else:
-            self.front = self.front.next
-            self.rear.next = self.front
-
-        self.size -= 1
+        self._s.remove_first()
         return True
 
     def deleteLast(self) -> bool:
         if self.isEmpty():
             return False
-
-        if self.front == self.rear:
-            self.front = None
-            self.rear = None
-        else:
-            current = self.front
-            while current.next != self.rear:
-                current = current.next
-            current.next = self.front
-            self.rear = current
-
-        self.size -= 1
+        self._s.remove_last()
         return True
 
     def getFront(self) -> int:
-        if self.isEmpty():
-            return -1
-        return self.front.value
+        return self._s.get_first() if not self.isEmpty() else -1
 
     def getRear(self) -> int:
-        if self.isEmpty():
-            return -1
-        return self.rear.value
+        return self._s.get_last() if not self.isEmpty() else -1
 
     def isEmpty(self) -> bool:
-        return self.size == 0
+        return self._s.is_empty()
 
     def isFull(self) -> bool:
-        return self.size == self.max_size
-
-
-# Stack Implementation using Singly Linked List
-class MyStack:
-    def __init__(self):
-        self.head = None
-
-    def push(self, x: int) -> None:
-        new_node = ListNode(x)
-        new_node.next = self.head
-        self.head = new_node
-
-    def pop(self) -> int:
-        if self.head is None:
-            return None
-        popped_value = self.head.value
-        self.head = self.head.next
-        return popped_value
-
-    def top(self) -> int:
-        if self.head is None:
-            return None
-        return self.head.value
-
-    def empty(self) -> bool:
-        return self.head is None
-
-
-# Lowest Common Ancestor (LCA) using Singly Linked List
-class Solution:
-    def lowestCommonAncestor(self, root: 'Node', p: 'Node', q: 'Node') -> 'Node':
-        current = root
-
-        # Traverse the tree
-        while current:
-            if p.val < current.val and q.val < current.val:
-                current = current.left
-            elif p.val > current.val and q.val > current.val:
-                current = current.right
-            else:
-                return current
+        return self._s.length() == self._K
