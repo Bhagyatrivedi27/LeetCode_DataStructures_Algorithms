44eee56451d830eb59ac106b844c0884ef974b4b
Bhagya Trivedi <53113456+Bhagyatrivedi27@users.noreply.github.com>
2024-10-13 01:15:29 -0400

    Time: 67 ms (12.68%), Space: 17.7 MB (13.07%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,101 +1,190 @@
-class ListNode:
+class ListNode:    
     def __init__(self, value=None):
         self.value = value
         self.next = None
 
-class MyCircularDeque:
+
+class MyCircularQueue:
     def __init__(self, k: int):
-        # Initialize the deque with a given maximum size
         self.max_size = k
         self.size = 0
-        self.front = None  # Front pointer of the deque
-        self.rear = None   # Rear pointer of the deque
-    
+        self.front = None
+        self.rear = None
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+
+        new_node = ListNode(value)
+
+        if self.isEmpty():
+            self.front = new_node
+            self.rear = new_node
+            new_node.next = self.front  
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+            self.rear.next = self.front 
+
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+
+        if self.front == self.rear:
+            self.front = None
+            self.rear = None
+        else:
+            self.front = self.front.next
+            self.rear.next = self.front
+
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.front.value
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.rear.value
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.max_size
+
+
+class MyCircularDeque:
+    def __init__(self, k: int):
+        self.max_size = k
+        self.size = 0
+        self.front = None
+        self.rear = None
+
     def insertFront(self, value: int) -> bool:
         if self.isFull():
             return False
-        
+
         new_node = ListNode(value)
-        
+
         if self.isEmpty():
-            # The first element, front and rear will point to the same node
             self.front = new_node
             self.rear = new_node
-            new_node.next = None
+            new_node.next = self.front  
         else:
-            # Add the new node to the front of the deque
             new_node.next = self.front
             self.front = new_node
-        
+            self.rear.next = self.front  
+
         self.size += 1
         return True
-    
+
     def insertLast(self, value: int) -> bool:
         if self.isFull():
             return False
-        
+
         new_node = ListNode(value)
-        
+
         if self.isEmpty():
-            # The first element, front and rear will point to the same node
             self.front = new_node
             self.rear = new_node
-            new_node.next = None
+            new_node.next = self.front  
         else:
-            # Add the new node at the rear of the deque
             self.rear.next = new_node
             self.rear = new_node
-        
+            self.rear.next = self.front  
+
         self.size += 1
         return True
-    
+
     def deleteFront(self) -> bool:
         if self.isEmpty():
             return False
-        
+
         if self.front == self.rear:
-            # Only one element in the deque
             self.front = None
             self.rear = None
         else:
-            # Remove the front node and update the front pointer
             self.front = self.front.next
-        
+            self.rear.next = self.front
+
         self.size -= 1
         return True
-    
+
     def deleteLast(self) -> bool:
         if self.isEmpty():
             return False
-        
+
         if self.front == self.rear:
-            # Only one element in the deque
             self.front = None
             self.rear = None
         else:
-            # Traverse the list to find the second-to-last node
             current = self.front
             while current.next != self.rear:
                 current = current.next
-            # Update rear and break the link to the last node
-            current.next = None
+            current.next = self.front
             self.rear = current
-        
+
         self.size -= 1
         return True
-    
+
     def getFront(self) -> int:
         if self.isEmpty():
             return -1
         return self.front.value
-    
+
     def getRear(self) -> int:
         if self.isEmpty():
             return -1
         return self.rear.value
-    
+
     def isEmpty(self) -> bool:
         return self.size == 0
-    
+
     def isFull(self) -> bool:
         return self.size == self.max_size
+
+
+class MyStack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, x: int) -> None:
+        new_node = ListNode(x)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self) -> int:
+        if self.head is None:
+            return None
+        popped_value = self.head.value
+        self.head = self.head.next
+        return popped_value
+
+    def top(self) -> int:
+        if self.head is None:
+            return None
+        return self.head.value
+
+    def empty(self) -> bool:
+        return self.head is None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'Node', p: 'Node', q: 'Node') -> 'Node':
+        current = root
+
+        # Traverse the tree
+        while current:
+            if p.val < current.val and q.val < current.val:
+                current = current.left
+            elif p.val > current.val and q.val > current.val:
+                current = current.right
+            else:
+                return current
